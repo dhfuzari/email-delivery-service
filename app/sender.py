3e59acc06b63592b1325be074b841a9d04b6b9be
Daniel Fuzari <daniel.fuzari@i-value.com.br>
2020-09-20 19:11:33 -0300

    feat(app): create a redis service to handle a mensage queue

--- app/sender.py
+++ app/sender.py
@@ -1,24 +1,35 @@
 import psycopg2
-from bottle import route, run, request
+import redis
+import json
+from bottle import Bottle, request
 
-DSN = 'dbname=email_sender user=postgres password=p@ssw0rd host=db'
-SQL = 'INSERT INTO emails(assunto, mensagem) VALUES(%s, %s)'
+class Sender(Bottle):
+    def __init__(self):
+        super().__init__()
+        self.route('/', method='POST', callback=self.send)
+        self.redisQueue = redis.StrictRedis(host='queue', port=6379, db=0)
+        DSN = 'dbname=email_sender user=postgres password=p@ssw0rd host=db'
+        self.conn = psycopg2.connect(DSN)
 
-def register_mensage(assunto, mensagem):
-    conn = psycopg2.connect(DSN)
-    cur = conn.cursor()
-    cur.execute(SQL, (assunto, mensagem))
-    conn.commit()
-    cur.close()
-    conn.close()
-    print('Mensagem registrada no banco')
+    def register_mensage(self, assunto, mensagem):
+        SQL = 'INSERT INTO emails(assunto, mensagem) VALUES(%s, %s)'
+        cur = self.conn.cursor()
+        cur.execute(SQL, (assunto, mensagem))
+        self.conn.commit()
+        cur.close()
+        
+        # Send mensage to redis
+        msg = { 'assunto': assunto, 'mensagem': mensagem }
+        self.redisQueue.rpush('sender', json.dumps(msg))
 
-@route('/', method='POST')
-def send():
-    assunto = request.forms.get('assunto')
-    mensagem = request.forms.get('mensagem')
-    register_mensage(assunto, mensagem)
-    return 'Mensagem enfileirada! Assunto: {} Mensagem: {}'.format(assunto, mensagem)
+        print('Mensagem registrada no SQL e no Redis')
+
+    def send(self):
+        assunto = request.forms.get('assunto')
+        mensagem = request.forms.get('mensagem')
+        self.register_mensage(assunto, mensagem)
+        return 'Mensagem enfileirada! Assunto: {} Mensagem: {}'.format(assunto, mensagem)
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080, debug=True)
+    sender = Sender()
+    sender.run(host='0.0.0.0', port=8080, debug=True)
